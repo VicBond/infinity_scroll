c09ae1e062be619f8ff65f5c22010fde4ff6bfec
Victor Bondarenko <nirasimha@gmail.com>
2022-06-08 11:46:00 -0400

    add async getPhotos func

--- app.js
+++ app.js
@@ -2,3 +2,20 @@
 const count = 10;
 const apiKey = 'mg9OhuTe-MEzbB1nJbtYEFacqS6zznYDDwapow8xEyA';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+//fetch photos from unsplash
+
+async function getPhotos(){
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log('data', data);
+  } catch (error) {
+    console.log('error', error);
+  }
+}
+
+
+//on load
+
+getPhotos();
