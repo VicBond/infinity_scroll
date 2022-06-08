91b9b7624ac495e6bf3a7527af83f6beb1d85eb9
Victor Bondarenko <nirasimha@gmail.com>
2022-06-08 11:42:18 -0400

    add unsplash API url

---
+++ app.js
@@ -0,0 +1,4 @@
+//unsplash API
+const count = 10;
+const apiKey = 'mg9OhuTe-MEzbB1nJbtYEFacqS6zznYDDwapow8xEyA';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
