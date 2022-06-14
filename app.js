f3c95320852ac354dec67313e2c5b436f477d1b0
Victor Bondarenko <nirasimha@gmail.com>
2022-06-14 09:37:41 -0400

    add displayPhotos func

--- app.js
+++ app.js
@@ -8,13 +8,18 @@ const count = 10;
 const apiKey = 'mg9OhuTe-MEzbB1nJbtYEFacqS6zznYDDwapow8xEyA';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+//create elements for links and photos => add to DOM
+function displayPhotos() {
+
+}
+
 //fetch photos from unsplash
 
 async function getPhotos(){
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log('data', data);
+    photosArray = await response.json();
+   
   } catch (error) {
     console.log('error', error);
   }
